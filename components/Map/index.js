315e0cfe0123af6657483b87f0aeb811c82333ad
Sofia Silva <sofiachaves.ps@outlook.com>
2021-04-14 23:19:48 +0100

    Protecting Key

--- components/Map/index.js
+++ components/Map/index.js
@@ -30,7 +30,7 @@ const Map = withScriptjs(withGoogleMap(({ sensors, selectedSensor, selectSensor 
 
 // Required by the package
 Map.defaultProps={    
-    googleMapURL:"https://maps.googleapis.com/maps/api/js?key=AIzaSyBgcwYsdYTicHgR3lr3PqGJC0dV4TkuMlA&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL:"https://maps.googleapis.com/maps/api/js?key=[GOOGLE_API_KEY]&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
